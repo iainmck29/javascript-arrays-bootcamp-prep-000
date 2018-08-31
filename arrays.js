bf660f8985aa08e0467dd085669ff5f1f308470b
Iain McKenzie <iainmck29@hotmail.com>
2018-08-31 09:42:29 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -22,3 +22,8 @@ function accessElementInArray (chocolateBars, index) {
   return (chocolateBars[index]);
 
 }
+
+function destructivelyRemoveElementFromBeginningOfArray(chocolateBars) {
+  chocolateBars.shift();
+  return chocolateBars
+}
