a27432d7770cfc1b486bf7a912e36c395190b1b1
Iain McKenzie <iainmck29@hotmail.com>
2018-08-31 09:05:23 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,6 +1,6 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
 
-function addElementToBeginningOfArray(){
+function addElementToBeginningOfArray(array, integer){
   var array = new Array();
   array.push();
   return array;
