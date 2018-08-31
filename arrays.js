f6969961d2a4f4fdf0b27875bbff5d042697a813
Iain McKenzie <iainmck29@hotmail.com>
2018-08-31 10:33:23 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -35,3 +35,8 @@ function destructivelyRemoveElementFromEndOfArray(chocolateBars) {
   chocolateBars.pop()
   return chocolateBars
 }
+
+function removeElementFromEndOfArray(chocolateBars) {
+  return chocolateBars.slice(-1)
+  
+}
